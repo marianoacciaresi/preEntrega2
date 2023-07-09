2217445e35e59d2bc6c2d76aad992429b1b85f5b
Mariano Acciaresi <maccia@gmail.com>
2023-07-09 15:45:58 -0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://marianoacciaresi.github.io/preEntrega1/'
+  base: 'https://marianoacciaresi.github.io/preEntrega2/'
 })
